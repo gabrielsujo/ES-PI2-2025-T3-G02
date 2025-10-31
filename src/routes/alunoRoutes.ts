5d5dee430b534254ccb7d63e33f6dfca953dea33
gabrielsuj <gm449870@gmail.com>
2025-10-31 18:02:12 -0300

    feat: Implmenta API principal (CRUD para Instituições, Disciplinas, Turmas, Alunos, Componentes e Notas)

---
+++ src/routes/alunoRoutes.ts
@@ -0,0 +1,126 @@
+import { Router } from 'express';
+import pool from '../config/db';
+
+const router = Router();
+
+const usuarioIdFixo = 1;
+
+router.get('/turmas/:turma_id/alunos', async (req, res) =>{
+    const { turma_id } = req.params;
+
+    try { 
+        const turmaCheck = await pool.query(
+            `SELECT t.id FROM turmas t
+            JOIN disciplinas d ON t.disciplina_id = d.id
+            JOIN instituicoes i ON d.instituicao_id = i.id
+            WHERE t.id = $1 i.usuario_id = $2`,
+            [turma_id, usuarioIdFixo]
+        );
+        if(turmaCheck.rows.length === 0) { 
+            return res.status(404).json({ error: 'Turmea não encontrada ou não autorizada.'});
+        }
+
+        const alunosResult = await pool.query(
+            'SELECT * FROM alunos WHERE turma_id = $1 ORDER BY nome',
+            [turma_id]
+        );
+
+        res.status(200).json(alunosResult.rows);
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ error: 'Erro interno do servidor.' });
+    }
+});
+
+router.post('/alunos', async(req, res) =>{
+    const { matricula, nome, turma_id } = req.body;
+
+    if (!matricula || !nome || !turma_id) {
+        return res.status(400).json({ error: 'Matrícula, nome e ID da turma são obrigatórios.' });
+    }
+
+    try {
+        const turmaCheck = await pool.query(
+            `SELECT t.id FROM turmas t
+            JOIN disciplinas d ON t.disciplina_id = d.id
+            JOIN instituicoes i ON d.instituicao_id = i.id
+            WHERE t.id = $1 AND i.usuario_id = $2`,
+            [turma_id, usuarioIdFixo]
+        );
+        if(turmaCheck.rows.length === 0) {
+            return res.status(403).json({ error: 'Ação não autorizada.'});
+        }
+
+        const novoAluno = await pool.query(
+            'INSERT INTO alunos (matricula, nome, turma_id) VALUES ($1, $2, $3) RETURNING *',
+            [matricula, nome, turma_id]
+        );
+
+        res.status(201).json(novoAluno.rows[0]);
+    } catch(err){
+        console.error(err);
+        res.status(500).json({ error: 'Erro interno do servidor.' });
+    }
+});
+
+router.put('/alunos/:id', async(req, res) => {
+    const { id } = req.params;
+    const { matricula, nome } = req.body;
+
+    if(!matricula || !nome) {
+        return res.status(400).json({ error: 'Matrícula e nome são obrigatórios. '});
+    }
+
+    try {
+        const alunoCheck = await pool.query(
+            `SELECT a.id FROM alunos a
+            JOIN turmas t ON a.turma_id = t.id
+            JOIN disciplinas d ON t.disciplina_id = d.id
+            JOIN instituicoes i ON d.instituicao_id = i.id
+            WHERE a.id = $1 i.usuario_id = $2`,
+            [id, usuarioIdFixo]
+        );
+
+        if (alunoCheck.rows.length === 0) {
+            return res.status(404).json({ error: 'Alunos não encontrados ou não autorizado.'});
+        }
+
+        const alunoAtualizado = await pool.query(
+            'UPDATE alunos SET matricula = $1, nome = $2 WHERE id = $3 RETURNING *',
+            [matricula, nome, id]
+        );
+
+        res.status(200).json(alunoAtualizado.rows[0]);
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ error: 'Erro interno do servidor'});
+    }
+});
+
+router.delete('/alunos/:id', async(req, res) => {
+    const { id } = req.params;
+
+    try {
+        const alunoCheck = await pool.query(
+            `SELECT a.id FROM alunos a
+            JOIN turmas t ON a.turma_id = t.id
+            JOIN disciplinas d ON t.disciplina_id = d.id
+            JOIN instituicoes i ON d.instiuicao_id = i.id
+            WHERE a.id = $1 AND i.usuario_id = $2`,
+            [id, usuarioIdFixo]
+        );
+
+        if(alunoCheck.rows.length === 0) {
+            return res.status(404).json({ error: 'Aluno não encontrado ou não autorizado.'});
+        }
+
+        await pool.query('DELETE FROM alunos WHERE id = $1', [id]);
+
+        res.status(200).json({ message: 'Aluno removido com sucesso.' });
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ error: 'Erro interno do servidor. Verifique se existem notas associadas.' });
+    }
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,11 @@ import path from 'path';
 
 //importar rotas e bancos
 import authRoutes from './routes/authRoutes';
-import instituicaoRoutes from './routes/instituicaoRoutes'
+import instituicaoRoutes from './routes/instituicaoRoutes';
+import disciplinaRoutes from './routes/diciplinaRoutes';
+import alunosRoutes from './routes/alunoRoutes';
+import componenteRoutes from './routes/componenteRoutes';
+import notaRoutes from './routes/notaRoutes';
 import pool from './config/db'
 
 const app = express();
@@ -17,9 +21,31 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/api', authRoutes);
 app.use('/api', instituicaoRoutes);
+app.use('/api', disciplinaRoutes);
+app.use('/api', alunosRoutes);
+app.use('/api', componenteRoutes);
+app.use('/api', notaRoutes);
 
+//rota principal
 app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
+})
+
+// outras rotas html
+app.get('/dashboard.html', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'dashboard.html'));
+});
+app.get('/disciplinas.html', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'disciplinas.html'));
+});
+app.get('/alunos.html', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'alunos.html'));
+});
+app.get('/componentes.html', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'componentes.html'));
+});
+app.get('/notas.html', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'notas.html'));
 });
 
 app.listen(port, () => {

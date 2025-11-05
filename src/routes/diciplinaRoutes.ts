af8e0083d83cbd8f4b1ed3af8a4a238f5e186e93
GuilhermeOlvr4 <gui.nipotec@gmail.com>
2025-11-05 18:29:51 -0300

    Update diciplinaRoutes.ts

--- src/routes/diciplinaRoutes.ts
+++ src/routes/diciplinaRoutes.ts
@@ -1,18 +1,23 @@
 import { Router } from 'express';
 import pool from '../config/db';
 import { Colors } from 'discord.js';
+import { authenticateToken, AuthRequest } from '../middlewares/authMiddleware';
 
 const router = Router();
 
-const usuarioIdFixo = 1;
 
-router.get('/instituicoes/:instituicao_id/disciplinas', async(req, res) =>{
+router.get('/instituicoes/:instituicao_id/disciplinas', authenticateToken, async(req: AuthRequest, res) =>{
     const { instituicao_id } = req.params;
+    const usuarioId = req.userId; 
+
+    if (!usuarioId) {
+        return res.status(403).json({ error: 'Autorização necessária.'});
+    }
 
     try {
         const instituicaoCheck = await pool.query(
-            'SELECT id FROM insitituicoes WHERE id = $1 AND usuario_id = $2',
-            [instituicao_id, usuarioIdFixo]
+            'SELECT id FROM instituicoes WHERE id = $1 AND usuario_id = $2',
+            [instituicao_id, usuarioId]
         );
         if (instituicaoCheck.rows.length === 0) {
             return res.status(401).json({ error: 'Instituição não encontrada ou não autorizada.'});
@@ -39,8 +44,13 @@ router.get('/instituicoes/:instituicao_id/disciplinas', async(req, res) =>{
     }
 });
 
-router.post( '/disciplinas', async (req, res) => {
+router.post( '/disciplinas', authenticateToken, async (req: AuthRequest, res) => {
     const { nome, sigla, codigo, periodo, instituicao_id } = req.body;
+    const usuarioId = req.userId; 
+
+    if (!usuarioId) {
+        return res.status(403).json({ error: 'Autorização necessária.'});
+    }
 
     if (!nome || !sigla || !codigo || !periodo || !instituicao_id) {
         return res.status(400).json({ error: 'Todos os campos são obrigatorios.' });
@@ -48,15 +58,15 @@ router.post( '/disciplinas', async (req, res) => {
 
     try{
         const instituicaoCheck = await pool.query (
-            'SELECT id FROM insituicoes WHERE id = $1  AND usuarios_id = $2',
-            [instituicao_id, usuarioIdFixo]
+            'SELECT id FROM instituicoes WHERE id = $1  AND usuarios_id = $2',
+            [instituicao_id, usuarioId]
         );
         if (instituicaoCheck.rows.length === 0) {
             return res.status(403).json({ error: 'Ação não autorizada.'});
         }
 
         const novaDiciplina = await pool.query(
-            'INSERT INTO disciplins (nome, sigla, codigo, periodo, instituicao_id VALUE ($1, $2, $3, $4, $5) RETURNING *',
+            'INSERT INTO disciplinas (nome, sigla, codigo, periodo, instituicao_id VALUE ($1, $2, $3, $4, $5) RETURNING *',
             [nome, sigla, codigo, periodo, instituicao_id]
         );
 
@@ -67,15 +77,20 @@ router.post( '/disciplinas', async (req, res) => {
     }
 });
 
-router.delete('/disciplinas/:id', async (req, res) => {
+router.delete('/disciplinas/:id', authenticateToken, async (req: AuthRequest, res) => {
     const { id } = req.params;
+    const usuarioId = req.userId; 
+
+    if (!usuarioId) {
+        return res.status(403).json({ error: 'Autorização necessária.'});
+    }
 
     try {
         const disciplinaCheck = await pool.query(
             `SELECT d.id FROM disciplinas d 
             JOIN instituicoes i ON d.instituicao_id = i.id 
             WHERE d.id = $1 AND i.usuario_id = $2`,
-            [id, usuarioIdFixo]
+            [id, usuarioId]
         );
 
         if (disciplinaCheck.rows.length === 0) {
@@ -91,8 +106,14 @@ router.delete('/disciplinas/:id', async (req, res) => {
     }
 });
 
-router.post('/turmas', async (req, res) =>{
+router.post('/turmas', authenticateToken, async (req: AuthRequest, res) =>{
     const { nome, dia, horario, local, disciplinaId } = req.body;
+    const usuarioId = req.userId; 
+
+    if (!usuarioId) {
+        return res.status(403).json({ error: 'Autorização necessária.'});
+    }
+    
 
     if (!nome || !dia || !horario || !local || !disciplinaId) {
         return res.status(400).json({ error: 'Campo obrigatório em falta.'});
@@ -101,9 +122,9 @@ router.post('/turmas', async (req, res) =>{
     try {
         const disciplinaCheck = await pool.query(
             `SELECT d.id FROM disciplinas d
-            JOIN insittuicoes i ON d.instituicao_id = i.id
+            JOIN instituicoes i ON d.instituicao_id = i.id
             WHERE d.id = $1 AND i.usuario_id = $2`,
-            [disciplinaId, usuarioIdFixo]
+            [disciplinaId, usuarioId]
         );
         if(disciplinaCheck.rows.length === 0) {
             return res.status(403).json({ error: 'Acão não autorizada.' });
@@ -121,23 +142,28 @@ router.post('/turmas', async (req, res) =>{
     }
 });
 
-router.delete('/turmas/:id', async(req, res) => {
+router.delete('/turmas/:id', authenticateToken, async(req: AuthRequest, res) => {
     const { id } = req.params;
+    const usuarioId = req.userId; 
+
+    if (!usuarioId) {
+        return res.status(403).json({ error: 'Autorização necessária.'});
+    }
 
     try{
         const turmaCheck = await pool.query(
             `SELECT t.id FROM turmas t
-            JOIN desciplinas d ON t.disciplina_id = d.id
+            JOIN disciplinas d ON t.disciplina_id = d.id
             JOIN instituicoes i ON d.instituicao_id = i.id
             WHERE t.id = $1 AND i.usuario_id = $2`,
-            [id, usuarioIdFixo]
+            [id, usuarioId]
         );
 
         if(turmaCheck.rows.length === 0) {
             return res.status(404).json({ error: 'turma não encontrada ou não autorizada.' });
         }
 
-        await pool.query('DELTE FROM turmas WHERE id = $1', [id]);
+        await pool.query('DELETE FROM turmas WHERE id = $1', [id]);
 
         res.status(200).json({ message: 'Turma removida com sucesso.' });
     } catch (err){

ac90d521e7d1f5513014262770c715810d61b5f1
mmoraes2005 <moraes.murilo2005@gmail.com>
2025-11-14 16:27:02 -0300

    Update codigo.sql
    removido a tabela matriculas
    adicionado dia_semana e local na tabela TURMAS
    senha para senha_hash

--- src/sql/codigo.sql
+++ src/sql/codigo.sql
@@ -3,14 +3,14 @@ create table Professores (
 	nome VARCHAR(255) NOT NULL,
 	telefone VARCHAR(20),
 	email VARCHAR(255) UNIQUE NOT NULL,
-	senha VARCHAR(255) NOT NULL
+	senha_hash VARCHAR(255) NOT NULL
 );
 create table Instituicoes (
 	id SERIAL PRIMARY KEY,
 	nome VARCHAR(255) NOT NULL,
 
-	professor_id INT NOT NULL,
-	FOREIGN KEY (professor_id) REFERENCES Professores(id)
+	usuario_id INT NOT NULL,
+	FOREIGN KEY (usuario_id) REFERENCES Professores(id)
 		ON DELETE CASCADE
 );
 create table Disciplinas (
@@ -39,6 +39,8 @@ create table Turmas (
 	nome VARCHAR(100) NOT NULL,
 	horario VARCHAR(100),
 	disciplina_id INT NOT NULL,
+	dia_semana VARCHAR(100),
+	local VARCHAR(255),
 	FOREIGN KEY (disciplina_id) REFERENCES Disciplinas(id)
 		ON DELETE CASCADE
 );
@@ -46,26 +48,17 @@ create table Alunos (
 	id SERIAL PRIMARY KEY,
 	nome VARCHAR(255) NOT NULL,
 	matricula VARCHAR(50),
-	professor_id INT NOT NULL,
-	FOREIGN KEY (professor_id) REFERENCES Professores(id)
+	turma_id INT NOT NULL,
+	FOREIGN KEY (turma_id) REFERENCES Turmas(id)
 		ON DELETE CASCADE
 );
-create table Matriculas (
-	id SERIAL PRIMARY KEY,
-	aluno_id int not null,
-	turma_id int not null,
-	FOREIGN KEY (aluno_id) REFERENCES Alunos(id) ON DELETE CASCADE,
-	foreign key (turma_id) REFERENCES Turmas(id) ON DELETE CASCADE,
 
-	Unique(aluno_id, turma_id)
-);
 create table Notas (
 	id SERIAL PRIMARY KEY,
 	valor DECIMAL(4,2) NOT NULL,
 	componente_id INT NOT NULL,
-	matricula_id INT NOT NULL,
+	aluno_id INT NOT NULL,
 	FOREIGN KEY (componente_id) references Componentes(id) ON DELETE CASCADE,
-	foreign key (matricula_id) references Matriculas(id) ON DELETE CASCADE
-	
+	foreign key (aluno_id) references Alunos(id) ON DELETE CASCADE
+	);
 
-);
